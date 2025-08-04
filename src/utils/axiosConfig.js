da6db628564f1c4b7db3010d0377ba52ea13e429
naik-shradha <shradhasaburi1702@gmail.com>
2025-08-04 13:32:05 +0530

    changes

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -5,7 +5,7 @@ import { toast } from "react-toastify";
 
 
 const instance = axios.create({
-  baseURL: "process.env.REACT_APP_BACKEND_URL || http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_BACKEND_URL || "http://localhost:5000/api",
 });
 
 instance.interceptors.request.use(
